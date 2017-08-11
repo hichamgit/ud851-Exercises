83ffa45b1bf0d9bb4b7b8c4f473a567c982290bd
Hicham <hicham.ta@gmail.com>
2017-08-11 15:57:18 +0100

    T03.04-Exercise-WiringUpRecyclerView : my solution

--- Lesson03-Green-Recycler-View/T03.04-Exercise-WiringUpRecyclerView/app/src/main/java/com/example/android/recyclerview/MainActivity.java
+++ Lesson03-Green-Recycler-View/T03.04-Exercise-WiringUpRecyclerView/app/src/main/java/com/example/android/recyclerview/MainActivity.java
@@ -35,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        
+
         // done TODO (4) Use findViewById to store a reference to the RecyclerView in mNumbersList
         mNumbersList = (RecyclerView) findViewById(R.id.rv_numbers);
 
