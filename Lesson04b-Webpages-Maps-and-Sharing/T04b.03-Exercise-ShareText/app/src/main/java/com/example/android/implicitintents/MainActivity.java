ed5bf0fb4d49c67147f744b81eae5cf1d8241a63
Hicham <hicham.ta@gmail.com>
2017-08-24 10:23:16 +0100

    T04b.03-Exercise-ShareText : my solution

--- Lesson04b-Webpages-Maps-and-Sharing/T04b.03-Exercise-ShareText/app/src/main/java/com/example/android/implicitintents/MainActivity.java
+++ Lesson04b-Webpages-Maps-and-Sharing/T04b.03-Exercise-ShareText/app/src/main/java/com/example/android/implicitintents/MainActivity.java
@@ -18,6 +18,7 @@ package com.example.android.implicitintents;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.ShareCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Toast;
@@ -67,9 +68,10 @@ public class MainActivity extends AppCompatActivity {
      */
     public void onClickShareTextButton(View v) {
         // TODO (5) Specify a String you'd like to share
-
+        String s = "This is what I want to share";
         // TODO (6) Replace the Toast with shareText, passing in the String from step 5
-        Toast.makeText(this, "TODO: Share text when this is clicked", Toast.LENGTH_LONG).show();
+        //Toast.makeText(this, "TODO: Share text when this is clicked", Toast.LENGTH_LONG).show();
+        shareText(s);
     }
 
     /**
@@ -147,8 +149,16 @@ public class MainActivity extends AppCompatActivity {
     // Do steps 2 - 4 within the shareText method
 
         // TODO (2) Create a String variable called mimeType and set it to "text/plain"
-
         // TODO (3) Create a title for the chooser window that will pop up
-
         // TODO (4) Use ShareCompat.IntentBuilder to build the Intent and start the chooser
+        private void shareText (String textToShare){
+            String mimeType = "text/plain";
+            String title = "title";
+            ShareCompat.IntentBuilder.from(this)
+                    .setChooserTitle(title)
+                    .setType(mimeType)
+                    .setText(textToShare)
+                    .startChooser();
+
+        }
 }
