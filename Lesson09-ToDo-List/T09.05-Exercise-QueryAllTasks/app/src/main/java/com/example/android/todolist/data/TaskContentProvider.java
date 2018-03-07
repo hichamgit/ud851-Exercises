50c8d076c3a32906d6a350cfa80b5e86d908a9d5
hichamgit <hicham.ta@gmail.com>
2018-03-07 16:54:44 +0000

    My solution to T09.05-Exercise-QueryAllTasks

--- Lesson09-ToDo-List/T09.05-Exercise-QueryAllTasks/app/src/main/java/com/example/android/todolist/data/TaskContentProvider.java
+++ Lesson09-ToDo-List/T09.05-Exercise-QueryAllTasks/app/src/main/java/com/example/android/todolist/data/TaskContentProvider.java
@@ -120,15 +120,23 @@ public class TaskContentProvider extends ContentProvider {
     public Cursor query(@NonNull Uri uri, String[] projection, String selection,
                         String[] selectionArgs, String sortOrder) {
 
-        // TODO (1) Get access to underlying database (read-only for query)
+        // TODO done (1) Get access to underlying database (read-only for query)
+        final SQLiteDatabase db = mTaskDbHelper.getReadableDatabase();
 
-        // TODO (2) Write URI match code and set a variable to return a Cursor
+        // TODO done (2) Write URI match code and set a variable to return a Cursor
+        int match = sUriMatcher.match(uri);
+        Cursor retCursor;
 
-        // TODO (3) Query for the tasks directory and write a default case
-
-        // TODO (4) Set a notification URI on the Cursor and return that Cursor
-
-        throw new UnsupportedOperationException("Not yet implemented");
+        // TODO done (3) Query for the tasks directory and write a default case
+        switch (match) {
+            case TASKS :
+                retCursor = db.query(TABLE_NAME,projection, selection, selectionArgs, null, null, sortOrder);
+                break;
+            default: throw new UnsupportedOperationException("Unknown Uri : "+uri);
+        }
+        // TODO done (4) Set a notification URI on the Cursor and return that Cursor
+        retCursor.setNotificationUri(getContext().getContentResolver(), uri);
+        return retCursor;
     }
 
 
